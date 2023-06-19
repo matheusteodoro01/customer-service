e6d7e9ef3d0e71026ae2232288a07b72924993f9
M. Teodoro <matheus.teodoro@farmaciasapp.com.br>
2023-06-19 10:21:01 -0300

    refactor: remove dead code

--- tests/mocks/customer.ts
+++ tests/mocks/customer.ts
@@ -1,7 +1,7 @@
 import { Customer } from '@/domain/models';
 
 export const makeCustomer: Customer = {
-  id: '2b40d133-ec58-4337-b33b-3e67b868f116',
-  name: 'fulano',
+  id: 'customer:2b40d133-ec58-4337-b33b-3e67b868f116',
+  name: 'any_name',
   document: 2233344422,
 };
